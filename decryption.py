b8e4a3dc47ab02929cdb94b773028b6b3af221b6
pororoarx <kengarrido.legaspi@gmail.com>
2023-04-16 09:10:56 +0800

    Initialized a function and looped through each character

--- decryption.py
+++ decryption.py
@@ -9,13 +9,28 @@ class Decryption:
         self.input_str = input_str
 
     # Inside the class, create and initialize a function "decrypt" that takes an input string
+    def decrypt(self):
         # Create a variable called "output_str"
+        output_str = ""
         # Loop through each character in the input string
+        for i in range(len(self.input_str)):
             # If the character is an asterisk, add "a" to the output_str
+            if self.input_str[i] == '*':
+                output_str += 'a';
             # If the character is an ampersand, add "e" to the output_str
+            elif self.input_str[i] == '&':
+                output_str += 'e';
             # If the character is a hash symbol, add "i" to the output_str
+            elif self.input_str[i] == '#':
+                output_str += 'i';
             # If the character is a plus symbol, add "o" to the output_str
+            elif self.input_str[i] == '+':
+                output_str += 'o';
             # If the character is an exclamation point, add "u" to the output_str
+            elif self.input_str[i] == '!':
+                output_str += 'u';
+            else:
+                output_str += self.input_str[i]
         # Return the output_str
 
 # Create a new object of the Decryption class and the input string of the user
