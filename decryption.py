a8f365d6c2265535a6a23c177eeedd93a3413db8
pororoarx <kengarrido.legaspi@gmail.com>
2023-04-16 08:54:51 +0800

    Pseudocode

---
+++ decryption.py
@@ -0,0 +1,20 @@
+# Start of pseudocode
+
+# Ask the user to input a string
+
+# Create a class called "Decryption"
+
+    # Inside the class, create and initialize a function "decrypt" that takes an input string
+        # Create a variable called "output_str"
+        # Loop through each character in the input string
+            # If the character is an asterisk, add "a" to the output_str
+            # If the character is an ampersand, add "e" to the output_str
+            # If the character is a hash symbol, add "i" to the output_str
+            # If the character is a plus symbol, add "o" to the output_str
+            # If the character is an exclamation point, add "u" to the output_str
+        # Return the output_str
+
+# Create a new object of the Decryption class and the input string of the user
+# Call the decrypt function and create a variable called output_str to get the results or decrypted output
+
+# Print the outpput string
